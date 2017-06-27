23d7bc4f903e23c3e900c05974e37af59adc900c
Ryan Winchester <code@ryanwinchester.ca>
2017-06-26 23:30:13 -0700

    fix test desc

--- test/hedwig_youtube_test.exs
+++ test/hedwig_youtube_test.exs
@@ -3,7 +3,7 @@ defmodule Hedwig.Responders.YoutubeTest do
 
   @tag start_robot: true, name: "alfred", responders: [{Hedwig.Responders.Youtube, []}]
 
-  test "responds with the weather", %{adapter: adapter, msg: msg} do
+  test "responds with relevant youtube link", %{adapter: adapter, msg: msg} do
     send adapter, {:message, %{msg | text: "alfred yt - something"}}
     assert_receive {:message, %{text: text}}, 1000
     assert String.contains?(text, "https://youtu.be/")
